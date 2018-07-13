d492eb6eb58bf65e7eabf52e5764f86118817963
Gerasim Stanchev <gero@tutuf.com>
2018-07-13 17:07:20 +0300

    Treat xml's tag attributes as first-class citizens in the result Map

--- lib/xml_to_map/naive_map.ex
+++ lib/xml_to_map/naive_map.ex
@@ -4,8 +4,10 @@ defmodule XmlToMap.NaiveMap do
   Module to recursively traverse the output of erlsom.simple_form
   and produce a map.
 
-  erlsom uses character lists so this library converts them to 
+  erlsom uses character lists so this library converts them to
   Elixir binary string.
+
+  Attributes, if present, are defined as tag@attribute_name => attribute_value
   """
 
   def parse([value]) do
@@ -16,29 +18,32 @@ defmodule XmlToMap.NaiveMap do
   end
 
   def parse({name, attr, content}) do
+    attributes = Enum.reduce(attr, %{}, fn({attribute_name, attribute_value}, acc) ->
+      Map.put(acc, "#{name}@#{attribute_name}", to_string(attribute_value))
+    end)
     parsed_content = parse(content)
     case is_map(parsed_content) do
-      true -> 
-        %{to_string(name) => parsed_content |> Map.merge(attr_map(attr))}
+      true ->
+        %{to_string(name) => parsed_content |> Map.merge(attr_map(attr)) |> Map.merge(attributes)}
       false ->
-        %{to_string(name) => parsed_content}
+        %{to_string(name) => parsed_content} |> Map.merge(attributes)
     end
   end
 
   def parse(list) when is_list(list) do
-    parsed_list = Enum.map list, &({to_string(elem(&1,0)), parse(&1)}) 
-    Enum.reduce parsed_list, %{}, fn {k,v}, acc -> 
+    parsed_list = Enum.map list, &({to_string(elem(&1, 0)), parse(&1)})
+    Enum.reduce parsed_list, %{}, fn {k,v}, acc ->
       case Map.get(acc, k) do
-        nil -> Map.put_new(acc, k, v[k])
+        nil ->
+          (for {key, value} <- v, into: %{}, do: {key, value})
+          |> Map.merge(acc)
         [h|t] -> Map.put(acc, k, [h|t] ++ [v[k]])
         prev -> Map.put(acc, k, [prev] ++ [v[k]])
       end
-    end 
+    end
   end
 
   defp attr_map(list) do
     list |> Enum.map(fn {k,v} -> {to_string(k), to_string(v)} end) |> Map.new
   end
-
-
 end

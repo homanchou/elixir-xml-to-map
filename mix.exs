3cfa3074bfa4128ace7003b6aef4f3811a58b6d2
Homan Chou <homan@sellbrite.com>
2020-03-29 11:04:59 -0700

    made a breaking change so should have bumped major version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule XmlToMap.Mixfile do
   def project do
     [
       app: :elixir_xml_to_map,
-      version: "0.2.0",
+      version: "1.0.0",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
